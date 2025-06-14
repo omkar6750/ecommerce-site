9a0eadfe89f4aa621af8d3c8a70293078b500494
Omkar Pawar <pawaromkar1654@gmail.com>
2025-06-14 21:37:37 +0530

    debug mode off

--- server/app.py
+++ server/app.py
@@ -912,4 +912,4 @@ def data_table_products():
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()  
-        app.run(debug=True)
+        app.run(debug=False , host='0.0.0.0')
